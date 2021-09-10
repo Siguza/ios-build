2cf7de2eed4ce6fce5004c446af3b49a9dc6774b
Siguza <siguza@siguza.net>
2021-09-10 17:51:49 +0200

    New dep

--- libimobiledevice/build.sh
+++ libimobiledevice/build.sh
@@ -34,7 +34,7 @@ fi;
 
 targets=();
 if [ "$#" = 0 ]; then
-    targets=('libplist' 'libusbmuxd' 'libimobiledevice' 'libirecovery' 'idevicerestore' 'ideviceinstaller' 'libideviceactivation');
+    targets=('libplist' 'libimobiledevice-glue' 'libusbmuxd' 'libimobiledevice' 'libirecovery' 'idevicerestore' 'ideviceinstaller' 'libideviceactivation');
 else
     while [ "$#" -gt 0 ]; do
         x="$1";
@@ -83,6 +83,7 @@ if [ "${#targets[@]}" = 0 ]; then
 fi;
 
 urls=('https://github.com/libimobiledevice/libplist.git' \
+      'https://github.com/libimobiledevice/libimobiledevice-glue.git' \
       'https://github.com/libimobiledevice/libusbmuxd.git' \
       'https://github.com/libimobiledevice/libimobiledevice.git' \
       'https://github.com/libimobiledevice/libirecovery.git' \
@@ -127,7 +128,8 @@ for target in "${targets[@]}"; do
     elif [ "$target" = 'idevicerestore' ]; then
         mkdir -p "__siguza/openssl";
         ln -s "$SDK/usr/include/CommonCrypto/CommonCrypto.h" '__siguza/openssl/sha.h';
-        cflags+=("-I$(pwd)/__siguza" '-DCOMMON_DIGEST_FOR_OPENSSL=1' '-Dmutex_destroy=idr_mutex_destroy' '-Dthread_new=idr_thread_new' '-Dmutex_init=idr_mutex_init' '-Dthread_join=idr_thread_join' '-Dmutex_unlock=idr_mutex_unlock' '-Dmutex_lock=idr_mutex_lock' '-Dthread_alive=idr_thread_alive' '-Dthread_free=idr_thread_free' '-Dthread_once=idr_thread_once' '-Dsocket_connect_unix=idr_socket_connect_unix' '-Dsocket_create_unix=idr_socket_create_unix' '-Dsocket_receive_timeout=idr_socket_receive_timeout' '-Dsocket_accept=idr_socket_accept' '-Dsocket_connect=idr_socket_connect' '-Dsocket_shutdown=idr_socket_shutdown' '-Dsocket_peek=idr_socket_peek' '-Dsocket_receive=idr_socket_receive' '-Dsocket_create=idr_socket_create' '-Dsocket_close=idr_socket_close' '-Dsocket_set_verbose=idr_socket_set_verbose' '-Dsocket_send=idr_socket_send' '-Dsocket_check_fd=idr_socket_check_fd');
+        #cflags+=("-I$(pwd)/__siguza" '-DCOMMON_DIGEST_FOR_OPENSSL=1' '-Dmutex_destroy=idr_mutex_destroy' '-Dthread_new=idr_thread_new' '-Dmutex_init=idr_mutex_init' '-Dthread_join=idr_thread_join' '-Dmutex_unlock=idr_mutex_unlock' '-Dmutex_lock=idr_mutex_lock' '-Dthread_alive=idr_thread_alive' '-Dthread_free=idr_thread_free' '-Dthread_once=idr_thread_once' '-Dsocket_connect_unix=idr_socket_connect_unix' '-Dsocket_create_unix=idr_socket_create_unix' '-Dsocket_receive_timeout=idr_socket_receive_timeout' '-Dsocket_accept=idr_socket_accept' '-Dsocket_connect=idr_socket_connect' '-Dsocket_shutdown=idr_socket_shutdown' '-Dsocket_peek=idr_socket_peek' '-Dsocket_receive=idr_socket_receive' '-Dsocket_create=idr_socket_create' '-Dsocket_close=idr_socket_close' '-Dsocket_set_verbose=idr_socket_set_verbose' '-Dsocket_send=idr_socket_send' '-Dsocket_check_fd=idr_socket_check_fd');
+        cflags+=("-I$(pwd)/__siguza" '-DCOMMON_DIGEST_FOR_OPENSSL=1');
         ldflags+=('-lbz2');
         export libcurl_CFLAGS="-I$SDK/usr/include";
         export libcurl_LIBS='-lcurl';
